3280957fa8a4ddf2f571c90b2be1931589c4fc3b
Uwe L. Korn <uwe.korn@quantco.com>
2025-03-20 08:59:15 +0100

    New filename pattern

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,8 @@
 @echo on
+
+set "PY_VER_PLAIN=!PY_VER:.=!"
+
 %PYTHON% -m pymsbuild wheel
 if errorlevel 1 exit 1
-%PYTHON% -m pip install dist\pymsbuild-%PKG_VERSION%-py3-none-any.whl
+%PYTHON% -m pip install dist\pymsbuild-%PKG_VERSION%-cp%PY_VER_PLAIN%-cp%PY_VER_PLAIN%-win_amd64.whl
 if errorlevel 1 exit 1
